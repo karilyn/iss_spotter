b0a2bfa8377032e3c8a42bb03d4abcb9a3fa1fc1
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-19 23:24:17 +0000

    commented out tests not currently required

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -16,5 +17,14 @@ const { fetchCoordsByIP } = require('./iss');
 //     return;
 //   }
 //   console.log(data);
+// });
 
-// })
+// const myCoords = { latitude: 50.998115, longitude: -118.195672 }
+// fetchISSFlyOverTimes(myCoords, (error, data) => {
+//   if (error) {
+//     console.log("Didn't work!", error);
+//     return;
+//   }
+//   console.log('Success! ISS flyover times: ', data);
+// });
+
