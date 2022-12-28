bfa11e37cff8323583c72e42743d980b44656e41
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-12-28 22:57:28 +0000

    implemented promise-based solution to ISS spotter

---
+++ index2.js
@@ -0,0 +1,15 @@
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
